38c58c401fac3f8ad7d92d6cc162f887ebcf766d
Murad Məmmədli <memmedleemurad@gmail.com>
2019-10-03 15:42:36 +0400

    Full Test

--- src/main/java/com/litse/dbservice/swagger/SwaggerConfig.java
+++ src/main/java/com/litse/dbservice/swagger/SwaggerConfig.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseMessageBuilder;
-import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.ResponseMessage;
@@ -14,7 +13,6 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
