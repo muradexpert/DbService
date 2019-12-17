e5fc55655e2df449108b2828f22ef2efb5b8dff2
Murad Məmmədli <memmedleemurad@gmail.com>
2019-12-17 16:01:34 +0400

    DB

--- src/test/java/com/litse/dbservice/DbServiceApplicationTests.java
+++ src/test/java/com/litse/dbservice/DbServiceApplicationTests.java
@@ -1,16 +1,17 @@
-package com.litse.dbservice;
-
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.verify;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class DbServiceApplicationTests {
-
-
-
-}
+//package com.litse.dbservice;
+//
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import static org.junit.Assert.assertEquals;
+//import static org.mockito.Mockito.verify;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class DbServiceApplicationTests {
+//
+//
+//
+//
+//}
